39086b23a9898b60fd7c7273b6d60ed62d0b8c59
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-09-12 20:43:15 +1000

    update.env

--- lib/actions.ts
+++ lib/actions.ts
@@ -16,13 +16,13 @@ import { ProjectForm } from "@/common.types";
 const isProduction = process.env.NODE_ENV === "production";
 const apiUrl = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
-  : "https://grafbaseflexibble-main-macr0dose.grafbase.app/graphql"
+  : "http://127.0.0.1:4000/graphql";
 const apiKey = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
-  : "letmein"
+  : "letmein";
   const serverUrl = isProduction
   ? process.env.NEXT_PUBLIC_SERVER_URL
-  : "http://localhost:3000"
+  : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
 
