a58df812b862b8b5664661dc637e7a8c93bffd08
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-09-12 14:58:45 +1000

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
 type SearchParams = {
-  category?: string | null;
-  endcursor?: string | null;
+  category?: string;
+  endCursor?: string;
 }
 
 type Props = {
