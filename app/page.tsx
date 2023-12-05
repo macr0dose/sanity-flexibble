93e1b38ccdb324b8782d674d0259c621e1c18e65
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-05 20:43:44 +1000

    add loading skeleton to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,34 @@
-"use client"
-
-import React, { useState, useEffect } from 'react';
+"use client">
+import { useState, useEffect } from "react";
 import Categories from "@/components/Categories";
 import LoadMore from "@/components/LoadMore";
-import { fetchAllProjects } from "@/lib/actions";
 import ProjectCard from "@/components/ProjectCard";
-
-// Skeleton Loader Component
-const ProjectCardSkeleton = () => {
-  return (
-    <div className="animate-pulse flex flex-col space-y-4 p-4 border border-gray-300 shadow rounded-md max-w-sm w-full mx-auto">
-      <div className="rounded bg-gray-300 h-32 w-full"></div>
-      <div className="space-y-3">
-        <div className="h-4 bg-gray-300 rounded"></div>
-        <div className="h-4 bg-gray-300 rounded w-5/6"></div>
-      </div>
-    </div>
-  );
-};
+import { fetchAllProjects } from "@/lib/actions";
+import { ProjectInterface } from "@/common.types";
+import SkeletonLoader from "./SkeletonLoader"; // Import the SkeletonLoader component
 
 type SearchParams = {
   category?: string;
   endCursor?: string;
 }
 
-type HomeProps = {
-  searchParams: SearchParams;
+type Props = {
+  searchParams: SearchParams
 }
 
-const Home = ({ searchParams: { category, endCursor } }: HomeProps) => {
+type ProjectSearch = {
+  projectSearch: {
+    edges: { node: ProjectInterface }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
+    };
+  };
+};
+
+const Home = async ({ searchParams: { category, endCursor } }: Props) => {
   const [loading, setLoading] = useState(true);
   const [projectsToDisplay, setProjectsToDisplay] = useState([]);
   const [pagination, setPagination] = useState({});
@@ -36,52 +36,55 @@ const Home = ({ searchParams: { category, endCursor } }: HomeProps) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = (await fetchAllProjects(category, endCursor)) as ProjectSearchData;
+        const data = (await fetchAllProjects(category, endCursor)) as ProjectSearch;
         setProjectsToDisplay(data.projectSearch.edges || []);
         setPagination(data.projectSearch.pageInfo || {});
         setLoading(false);
       } catch (error) {
-        // Handle error
+        console.error(error);
+        setLoading(false);
       }
     };
-
     fetchData();
   }, [category, endCursor]);
+
+  if (loading) {
+    return (
+      <section className="flex-start flex-col paddings">
+        <Categories />
+        <div className="projects-grid">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <SkeletonLoader key={index} />
+          ))}
+        </div>
+      </section>
+    );
+  }
 
   return (
     <section className="flex-start flex-col paddings mb-16">
       <Categories />
 
-      {loading ? (
-        <div className="projects-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {Array.from({ length: 6 }).map((_, index) => (
-            <ProjectCardSkeleton key={index} />
-          ))}
-        </div>
-      ) : (
-        <>
-          <section className="projects-grid">
-            {projectsToDisplay.map(({ node }) => (
-              <ProjectCard
-                key={node.id}
-                id={node.id}
-                image={node.image}
-                title={node.title}
-                name={node.createdBy.name}
-                avatarUrl={node.createdBy.avatarUrl}
-                userId={node.createdBy.id}
-              />
-            ))}
-          </section>
-
-          <LoadMore
-            startCursor={pagination.startCursor}
-            endCursor={pagination.endCursor}
-            hasPreviousPage={pagination.hasPreviousPage}
-            hasNextPage={pagination.hasNextPage}
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={node?.id}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy?.name}
+            avatarUrl={node?.createdBy?.avatarUrl}
+            userId={node?.createdBy?.id}
           />
-        </>
-      )}
+        ))}
+      </section>
+
+      <LoadMore
+        startCursor={pagination.startCursor}
+        endCursor={pagination.endCursor}
+        hasPreviousPage={pagination.hasPreviousPage}
+        hasNextPage={pagination.hasNextPage}
+      />
     </section>
   );
 };
---
+++ components/SkeletonLoader.tsx
@@ -0,0 +1,13 @@
+const SkeletonLoader = () => {
+    return (
+      <div className="animate-pulse space-y-4">
+        <div className="h-4 bg-gray-300 rounded w-3/4"></div>
+        <div className="space-y-2">
+          <div className="h-4 bg-gray-300 rounded"></div>
+          <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default SkeletonLoader;
